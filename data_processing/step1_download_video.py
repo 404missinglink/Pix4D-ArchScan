03665dde75687a621eb387e37bdae265db2e4c48
sd2879 <suman8deb@gmail.com>
2024-10-05 23:53:12 +0100

    update v2

--- data_processing/step1_download_video.py
+++ data_processing/step1_download_video.py
@@ -2,10 +2,8 @@ import os
 import subprocess
 import time
 
-def download_video(youtube_link, outpath_folder, cookies_path="cookies.txt"):
-    start_time = time.time()  # Record start time
-    video_id = youtube_link.split('=')[-1]
-    output_path = os.path.join(outpath_folder, f"{video_id}.mp4")
+def download_video(youtube_link, output_path, cookies_path="cookies.txt"):
+    start_time = time.time()
     command = [
         "yt-dlp",
         "--cookies", cookies_path,
@@ -14,9 +12,8 @@ def download_video(youtube_link, outpath_folder, cookies_path="cookies.txt"):
         "-o", output_path
     ]
     result = subprocess.run(command, capture_output=True, text=True)
-    end_time = time.time()  # Record end time
+    end_time = time.time()
     
-    # Calculate and print the time taken
     time_taken = end_time - start_time
     print(f"Time taken to download the video: {time_taken:.2f} seconds")
     
@@ -27,9 +24,8 @@ def download_video(youtube_link, outpath_folder, cookies_path="cookies.txt"):
         print(f"Error downloading video: {result.stderr}")
         return None
 
-# Define the YouTube link and output folder
 youtube_link = "https://www.youtube.com/watch?v=rkfFCSbWDyY"
 outpath_folder = "output"
-
-# Download the video and print time taken
-downloaded_video_path = download_video(youtube_link, outpath_folder)
+video_id = youtube_link.split('=')[-1]
+output_path = os.path.join(outpath_folder, f"{video_id}.mp4")
+downloaded_video_path = download_video(youtube_link, output_path)
